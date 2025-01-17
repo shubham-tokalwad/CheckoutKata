f51e413ba615593b23daea2433837b3a5deff553
ShubhamTokalwad <shubhamtokalwad16@gmail.com>
2025-01-17 00:43:54 +0000

    Added more conditions tests

--- Kata.Tests/CheckoutTests.cs
+++ Kata.Tests/CheckoutTests.cs
@@ -47,5 +47,71 @@ namespace Kata.Tests
             // Assert
             Assert.Equal(0.80m, total);
         }
+
+        [Fact]
+        public void Scan_ApplyOffers()
+        {
+            // Arrange
+            var items = new List<Item>
+            {
+                new Item("A99", 0.50m),
+                new Item("B15", 0.30m)
+            };
+
+            var offers = new List<SpecialOffer>
+            {
+                new SpecialOffer("A99", 3, 1.30m),
+                new SpecialOffer("B15", 2, 0.45m)
+            };
+
+            var pricingService = new PricingService(items, offers);
+            var checkout = new Checkout(pricingService);
+
+            // Act
+            checkout.Scan("A99");
+            checkout.Scan("A99");
+            checkout.Scan("A99");
+            checkout.Scan("B15");
+            checkout.Scan("B15");
+
+            var total = checkout.GetTotal();
+
+            // Assert
+            Assert.Equal(1.75m, total);
+        }
+
+        [Fact]
+        public void Scan_WithAndWithoutOffers()
+        {
+            // Arrange
+            var items = new List<Item>
+            {
+                new Item("A99", 0.50m),
+                new Item("B15", 0.30m),
+                new Item("C40", 1.20m)
+            };
+
+            var offers = new List<SpecialOffer>
+            {
+                new SpecialOffer("A99", 3, 1.30m),
+                new SpecialOffer("B15", 2, 0.45m)
+            };
+
+            var pricingService = new PricingService(items, offers);
+            var checkout = new Checkout(pricingService);
+
+            // Act
+            checkout.Scan("A99");
+            checkout.Scan("A99");
+            checkout.Scan("A99");
+            checkout.Scan("B15");
+            checkout.Scan("B15");
+            checkout.Scan("C40");
+
+            var total = checkout.GetTotal();
+
+            // Assert
+            Assert.Equal(2.95m, total);
+        }
     }
 }
